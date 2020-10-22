3041922ca7b2519436efd352c5b29698bb7fb657
Layrovell <marina95prodan@gmail.com>
2020-10-22 20:35:48 +0300

    slick slider

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+$(function () {
+  $(".slider-area").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    centerMode: true,
+    // cssEase: 'linear',
+    // fade: true,
+  });
+});
+
